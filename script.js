36c0c751fb24ddc956e6b2011cfa91280de078c6
nvquinter <valuquintero@gmail.com>
2023-12-20 21:05:37 -0300

    agregue archivo agenda

---
+++ script.js
@@ -0,0 +1,16 @@
+//creo la variable
+let persona= "Marta";
+//me la muestra en consola
+console.log(persona)
+//llamo y modifico la variable
+persona="Maria";
+//me  muestra el cambio en la consola
+console.log(persona)
+
+//operaciones
+console.log(10+33)
+let resultado= 300-11;
+console.log(resultado)
+//concatenacion unir(string)
+console.log("Hola" + "Pepe")
+console.log()
